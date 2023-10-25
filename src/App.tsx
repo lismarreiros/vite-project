5d4b9456c8e0c2d954e2f8a3ab24bc2074e3e990
lismarreiros <lismarreiros@hotmail.com>
2023-10-25 17:34:07 -0300

    telas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Viagens from './components/viagens';
 import DetViagem from './components/detalhes';
 import Form from './components/adicionar/Form'
 import NDespesaMobile from './components/detalhes/NDespesaMobile';
+import Signin from './components/auth/Signin';
+import Signup from './components/auth/Signup';
 
 
 
@@ -11,6 +13,8 @@ export default function App() {
     <BrowserRouter>
       <Routes>
       <Route path='/' element={<Form/>} />
+      <Route path='/signin' element={<Signin/>} />
+      <Route path='/signup' element={<Signup/>} />
       <Route path="/viagens" element={<Viagens/>} />
       <Route path='/detalhes' element={<DetViagem/>} />
       <Route path='/novadespesa' element={<NDespesaMobile/>} />
